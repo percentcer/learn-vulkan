843b7111726f6c6c1b9102231ae427070324d3c7
Spencer Miller <spence.miller@gmail.com>
2020-11-11 12:38:49 -0800

    Update project files to use $VULKAN_SDK, get rid of .props

--- learn-vulkan/compile_shaders.bat
+++ learn-vulkan/compile_shaders.bat
@@ -1,4 +1,4 @@
 if not exist ".\shaders_out" mkdir .\shaders_out
-C:\VulkanSDK\1.2.131.2\Bin\glslc.exe .\shaders\shader.vert -o .\shaders_out\vert.spv
-C:\VulkanSDK\1.2.131.2\Bin\glslc.exe .\shaders\shader.frag -o .\shaders_out\frag.spv
+%VULKAN_SDK%\Bin\glslc.exe .\shaders\shader.vert -o .\shaders_out\vert.spv
+%VULKAN_SDK%\Bin\glslc.exe .\shaders\shader.frag -o .\shaders_out\frag.spv
 pause
